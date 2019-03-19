4ebb238e01253e2761fde9e964792a77d68d31c1
wolfhaltz <beatriztattooer@gmail.com>
2019-03-19 18:04:52 -0300

    Discount calculator

---
+++ calculate_discount.py
@@ -0,0 +1,9 @@
+#How to calculate discount?
+
+value_product = float(input('Type the price of the product: R$'))
+
+value_discount = float(input('Type your discount:'))
+
+result = (value_product * value_discount / 100)
+
+print(result)
