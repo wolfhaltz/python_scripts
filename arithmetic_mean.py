2b1ce728fc8b8bf5b488fded8dbe569867fa2d5b
wolfhaltz <beatriztattooer@gmail.com>
2019-03-20 09:22:41 -0300

    Arithmetic mean

---
+++ arithmetic_mean.py
@@ -0,0 +1,7 @@
+#Arithmetic mean
+first_note = float(input('First note:'))
+second_note = float(input('Second note:'))
+
+average = (first_note + second_note)/2
+
+print('Your average is {}'.format(average))
---
+++ double_triple_square_root.py
@@ -0,0 +1,7 @@
+#Double, Triple, Square Root
+
+number = int(input('Type a number:'))
+double = number*2
+triple = number*3
+square_root = number**(1/2)
+print('Your number is {}.\nDouble:{}\nTriple:{}\nSquare Root:{:.2f}'.format(number, double, triple, square_root))
